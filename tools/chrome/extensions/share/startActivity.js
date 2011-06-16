62b9801866bb7bd8976fa359fc4e20c0e8159a18
Paul Kinlan <paulkinlan@google.com>
2011-06-16 13:39:07 +0100

    Changes for the Chrome Extension #4, it still isn't working but we are on better ground

---
+++ tools/chrome/extensions/share/startActivity.js
@@ -0,0 +1,6 @@
+
+var port = chrome.extension.connect();
+port.onMessage.addListener(function(intent) {
+  // There has been a share intent called
+  window.navigator.intents.startActivity(intent);
+});
