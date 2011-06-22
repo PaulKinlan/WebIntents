069dd5243810f2919f06806b784dcbd107443ab6
Paul Kinlan <paulkinlan@google.com>
2011-06-22 21:07:47 +0100

    #34 - tidying up for IE support

--- server/webintents-server.js
+++ server/webintents-server.js
@@ -123,7 +123,6 @@ var MessageDispatcher = function() {
 var MessageHandler = function() {
   var dispatcher = new MessageDispatcher();
   this.handler = function(e) {
-    console.log(e);
     var data;
     if(!!e.data) {
       data = JSON.parse(e.data);
