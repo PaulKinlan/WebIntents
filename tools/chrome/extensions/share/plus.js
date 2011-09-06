40706898b7d0fa0cd8bd59eddf9a4a7736d05f99
Paul Kinlan <paulkinlan@google.com>
2011-09-06 16:29:55 -0700

    #4 making share work over https

--- tools/chrome/extensions/share/plus.js
+++ tools/chrome/extensions/share/plus.js
@@ -1,26 +1,26 @@
-var shareElement = document.getElementById(":3r.f");
-
-if(window.intent) {
-  shareElement.innerText = window.intent.data;
-}
-
 var addIntent = function() {
   // Always add the intent in.
   var intentEl = document.createElement("intent");
   intentEl.setAttribute("action", "http://webintents.org/share");
   intentEl.setAttribute("type", "text/uri-list");
+  intentEl.setAttribute("href", document.location.href);
   document.head.appendChild(intentEl);
 };
 
 var checkIntent = function() {
-  if(window.intent) {
-  }
-};
+    setTimeout(function() {
+      if (!!window.intent == false) return;
+      var shareElement = document.getElementById(":3r.f");
+      se.innerText = window.intent.data;
+      se.focus();
+    }, 1000);
+}
 
 if(document.readyState == "complete") {
   addIntent();
-
+  checkIntent();
 }
 else {
   window.addEventListener("load", addIntent, false);
+  window.addEventListener("load", checkIntent, false);
 }
