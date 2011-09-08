1d26befa64ef3d8363a0c6e77e83b28c10ea52ce
Tony Payne <payneinthe@gmail.com>
2011-09-08 16:14:27 -0700

    added google picker

---
+++ server/ssl/SimpleSecureHTTPServer.py
@@ -0,0 +1,31 @@
+import BaseHTTPServer, SimpleHTTPServer
+import ssl
+import sys
+import urlparse
+import httplib2
+
+class FetchImageHandler(SimpleHTTPServer.SimpleHTTPRequestHandler):
+  def do_GET(self):
+    if (self.path.find("/proxy") == 0):
+      url = urlparse.parse_qs(urlparse.urlparse(self.path).query)['url'][0]
+      response, content = httplib2.Http().request(url, headers = {
+          'Connection': 'close'})
+      self.send_response(int(response['status']))
+      self.send_header('Content-Type', response['content-type'])
+      self.send_header('Content-Length', response['content-length'])
+      self.send_header('Connection', 'close')
+      self.end_headers()
+      self.wfile.write(content)
+    else:
+      SimpleHTTPServer.SimpleHTTPRequestHandler.do_GET(self)
+
+
+def main():
+  port = int(sys.argv[1])
+  httpd = BaseHTTPServer.HTTPServer(('', port), FetchImageHandler)
+  httpd.socket = ssl.wrap_socket (httpd.socket, certfile='../server/ssl/server.pem', server_side=True)
+  print "Serving on", port
+  httpd.serve_forever()
+
+if __name__ == "__main__":
+  main()
