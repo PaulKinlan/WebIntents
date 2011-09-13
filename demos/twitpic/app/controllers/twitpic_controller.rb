be7e99516ad191aa1b4ebcd8bce7adf9a3b78e3b
Tony Payne <payneinthe@gmail.com>
2011-09-13 11:49:02 +0800

    extracted image fetching to library

--- demos/twitpic/app/controllers/twitpic_controller.rb
+++ demos/twitpic/app/controllers/twitpic_controller.rb
@@ -1,4 +1,5 @@
 require 'datafy'
+require 'image_fetch'
 require 'open-uri'
 require 'uri'
 
@@ -27,18 +28,11 @@ class TwitpicController < ApplicationController
   end
 
   def upload
-    url = request.params['img'] || ''
-    path = if url.starts_with?('data:')
-      store_data_url(url)
-    elsif url.starts_with?('http://') || url.starts_with?('https://')
-      store_url(url)
-    else
-      raise 'Unsupported URL format'
-    end
+    image = ImageFetch.new(request.params['img'])
     begin
-      render :json => twitpic_client.upload(path, '')
+      render :json => twitpic_client.upload(image.path, '')
     ensure
-      cleanup(path)
+      image.cleanup
     end
   end
 
@@ -48,42 +42,4 @@ class TwitpicController < ApplicationController
   end
 
   private
-  def store_data_url(url)
-    data, type = Datafy::decode_data_uri(url)
-    suffix = get_suffix(type)
-    return get_tempfile('image' + suffix) { |f|
-      f.write(data)
-    }
-  end
-
-  def store_url(url)
-    return get_tempfile(File::basename(URI.split(url)[5])) { |f|
-      f << open(url).read
-    }
-  end
-
-  def get_tempfile(filename, &block)
-    dir = File.join(Dir::tmpdir, "twitpic_#{Process.pid}_#{rand(100)}_#{Time.now.to_i}")
-    Dir.mkdir(dir)
-    filepath = File.join(dir, filename)
-    File.open(filepath, "wb", &block)
-    return filepath
-  end
-
-  def cleanup(filepath)
-    File.unlink(filepath)
-    Dir.rmdir(File::dirname(filepath))
-  end
-
-  def get_suffix(type)
-    if (type == 'image/jpg' || type == 'image/jpeg')
-      return '.jpg'
-    elsif (type == 'image/png')
-      return '.png'
-    elsif (type == 'image/gif')
-      return '.gif'
-    else
-      raise 'Unsupported image type'
-    end
-  end
 end
