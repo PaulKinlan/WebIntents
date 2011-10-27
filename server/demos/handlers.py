df2dcf53c6390824ee3716a5f1fa248e359cd863
Paul Kinlan <paulkinlan@google.com>
2011-10-27 20:20:55 +0100

    Fixes for file paths #111

--- server/demos/handlers.py
+++ server/demos/handlers.py
@@ -11,11 +11,10 @@ class PageHandler(webapp2.RequestHandler):
   def get(self, file):
     if file is None or file == "":
       file = "index.html"
-
     # test if the file exists
-    path = os.path.join("pages", file)
+    path = os.path.join(os.path.dirname(__file__), "pages", file)
     if os.path.exists(path):
-      template = jinja_environment.get_template(path)
+      template = jinja_environment.get_template(os.path.join("pages", file))
       self.response.out.write(template.render())
     else:
       self.error(404)
