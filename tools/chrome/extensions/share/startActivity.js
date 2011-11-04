6b9c88ddc68bc714ce506c236595b76ae2c76dc4
Paul Kinlan <paulkinlan@google.com>
2011-11-04 10:41:24 +0000

    Embeding a new script block to start the activity rather than the content_script

--- tools/chrome/extensions/share/startActivity.js
+++ tools/chrome/extensions/share/startActivity.js
@@ -1,3 +1,10 @@
 chrome.extension.onRequest.addListener(function(intent) {
-  window.navigator.startActivity(intent);
+    var script = "";
+    script += "var i = new Intent('" + intent.action + "'");
+    script += ",'" + intent.type + "',";
+    script += ",JSON.parse('" + JSON.stringify(intent.data) + "');";
+    script += "window.navigator.startActivity(i);";
+    var scriptElement = document.createElement(scriptElement);
+    scriptElement.innerText = script;
+    document.head.appendChild(scriptElement);
 });
