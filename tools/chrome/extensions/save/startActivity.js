25a4ccc2591b40843fdeb4ac9cdb53ea14f920ce
Paul Kinlan <paulkinlan@google.com>
2011-11-04 16:31:42 +0000

    Adding more save extension

---
+++ tools/chrome/extensions/save/startActivity.js
@@ -0,0 +1,10 @@
+chrome.extension.onRequest.addListener(function(intent) {
+    var script = "";
+    script += "var i = new Intent('" + intent.action + "'";
+    script += ",'" + intent.type + "'";
+    script += ", JSON.parse('" + JSON.stringify(intent.data) + "'));";
+    script += "window.navigator.startActivity(i);";
+    var scriptElement = document.createElement(scriptElement);
+    scriptElement.innerText = script;
+    document.head.appendChild(scriptElement);
+});
