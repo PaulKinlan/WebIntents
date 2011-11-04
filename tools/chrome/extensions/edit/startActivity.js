e81db2fd7a90bc7a8c2b8850f4cf48977b422bfd
Paul Kinlan <paulkinlan@google.com>
2011-11-04 17:10:54 +0000

    Fixing an issue with startActivity

--- tools/chrome/extensions/edit/startActivity.js
+++ tools/chrome/extensions/edit/startActivity.js
@@ -4,7 +4,7 @@ chrome.extension.onRequest.addListener(function(intent) {
     script += ",'" + intent.type + "'";
     script += ", JSON.parse('" + JSON.stringify(intent.data) + "'));";
     script += "window.navigator.startActivity(i);";
-    var scriptElement = document.createElement(script);
+    var scriptElement = document.createElement("script");
     scriptElement.innerText = script;
     document.head.appendChild(scriptElement);
 });
