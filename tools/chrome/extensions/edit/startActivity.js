8a692582de22961893c9d453ca9830a9571a246a
Paul Kinlan <paulkinlan@google.com>
2011-11-04 14:31:55 +0000

    Fixing a syntax error in startActivity

--- tools/chrome/extensions/edit/startActivity.js
+++ tools/chrome/extensions/edit/startActivity.js
@@ -1,7 +1,7 @@
 chrome.extension.onRequest.addListener(function(intent) {
     var script = "";
-    script += "var i = new Intent('" + intent.action + "'");
-    script += ",'" + intent.type + "',";
+    script += "var i = new Intent('" + intent.action + "'";
+    script += ",'" + intent.type + "'";
     script += ", JSON.parse('" + JSON.stringify(intent.data) + "'));";
     script += "window.navigator.startActivity(i);";
     var scriptElement = document.createElement(scriptElement);
