5b121cb6eee3396bb7dce48ae6b787b5381efc9b
Paul Kinlan <paulkinlan@google.com>
2011-11-06 22:53:58 +0000

    #111 Fixing Imgur demo

--- server/demos/imgur/handlers.py
+++ server/demos/imgur/handlers.py
@@ -45,9 +45,13 @@ class PageHandler(webapp2.RequestHandler):
 class SaveHandler(webapp2.RequestHandler):
   def post(self):
     url = "http://api.imgur.com/2/upload.json"
-    payload = { "key": "41bd72340ae809a19c392d88028522a4",  "image": "%s" % self.request.get("url") }
+    payload = { 
+        "key": "41bd72340ae809a19c392d88028522a4",  
+        "image": "%s" % self.request.body, 
+        "type": "%s" % self.request.get("type") }
     fetchdata = urlfetch.fetch(url, method = "POST", payload = urllib.urlencode(payload) )
     self.response.headers['Content-type'] = fetchdata.headers['Content-type']
     self.response.set_status(fetchdata.status_code)
+    logging.info(fetchdata.content)
     self.response.out.write(fetchdata.content)
     
