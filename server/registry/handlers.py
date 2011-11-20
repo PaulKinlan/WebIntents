94189d8d8fcaf2b612e202decad1e6045ebc5b6a
Paul Kinlan <paulkinlan@google.com>
2011-11-20 08:21:32 +0100

    Refactoring handlers

--- server/registry/handlers.py
+++ server/registry/handlers.py
@@ -3,18 +3,7 @@ import jinja2
 import os
 from google.appengine.api import urlfetch
 from google.appengine.ext import db
-
-
-
-jinja_environment = jinja2.Environment(
-        loader=jinja2.FileSystemLoader(
-          os.path.dirname(__file__)))
-
-file_types = {
-  '.js' : 'application/javascript',
-  '.html' : 'text/html',
-  '.css' : 'text/css'
-} 
+import handlers
 
 class Intent(db.Model):
   href = db.StringProperty(indexed = False)
@@ -32,32 +21,10 @@ class IndexHistory(db.Model):
   created_on = db.DateTimeProperty(auto_now = False, auto_now_add = True)
   status = db.TextProperty()
 
-class PageHandler(webapp2.RequestHandler):
-  def get(self, file):
-    self.response.headers['X-Content-Security-Policy'] = "allow 'self'; img-src *; script-src www.google-analytics.com apis.google.com;"
-    
-    if file is None or file == "":
-      file = "index.html"
-
-    name, extension  = os.path.splitext(file)
-
-    content_type = file_types.get(extension, "text/html") 
-    self.response.headers['Content-Type'] = content_type
-
-    # test if the file exists in the static
-    path = os.path.join(os.path.dirname(__file__), "static", file)
-    if os.path.exists(path):
-      f = open(path, "r")
-      self.response.out.write(f.read())
-      return
-    
-    path = os.path.join(os.path.dirname(__file__), "pages", file)
-    if os.path.exists(path):
-      template = jinja_environment.get_template(os.path.join("pages", file))
-      self.response.out.write(template.render())
-    else:
-      self.error(404)
-
+class PageHandler(handlers.PageHandler):
+   """
+   The base page handler for the registry
+   """
 
 class IndexerHandler(PageHandler):
   def post(self):
@@ -110,4 +77,3 @@ class RegisteryHandler(webapp2.RequestHandler):
       self.response.out.write(template.render({intents = results}))
     else:
       self.error(404)
-
