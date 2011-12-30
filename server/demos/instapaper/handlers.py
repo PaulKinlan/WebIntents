a6f5cab8a668af945fb4f3e7695641914a8226e3
Paul Kinlan <paulkinlan@google.com>
2011-12-30 12:04:56 +0000

    Fixes #141 - adds missing imports and other fixes

--- server/demos/instapaper/handlers.py
+++ server/demos/instapaper/handlers.py
@@ -1,5 +1,7 @@
 import webapp2
 from google.appengine.api import urlfetch
+import logging
+import urllib
 
 class AddHandler(webapp2.RequestHandler):
   def post(self):
@@ -12,5 +14,7 @@ class AddHandler(webapp2.RequestHandler):
     }
     fetchdata = urlfetch.fetch(url, method = "POST", payload = urllib.urlencode(body))
     self.response.headers['Content-type'] = fetchdata.headers['Content-type']
+    self.response.headers['Content-Location'] = fetchdata.headers['Content-Location']
+    self.response.headers['X-Instapaper-Title'] = fetchdata.headers['X-Instapaper-Title']
     self.response.set_status(fetchdata.status_code)
     self.response.out.write(fetchdata.content)
