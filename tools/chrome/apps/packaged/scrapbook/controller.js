1b0e8eee7b651bdc1a2d660b6181d05047924d94
Paul Kinlan <paulkinlan@google.com>
2012-01-16 17:52:48 -0800

    #148 - adding lots of code to the beautiful scrap book

---
+++ tools/chrome/apps/packaged/scrapbook/controller.js
@@ -0,0 +1,58 @@
+var Scrappy = new (function() {
+  this.save = function(data, name, type) {
+    filer.init({persistent: true, size: size}, function(fs) {
+      filer.write(name, {data: data, type: type}, function(fileEntry) {
+        fileList.addFile(fileEntry);
+      }, function(e) {console.log(e)});
+    }, function(e) { console.log(e)});
+  };
+
+  this.startShare = function() {
+    var file = fileList.file;
+    var intent = new Intent("http://webintents.org/share", file.type, file);
+    window.navigator.startActivity(intent);
+  };
+
+  this.startSave = function() {
+    var file = fileList.file;
+    var intent = new Intent("http://webintents.org/share", file.type, file);
+    window.navigator.startActivity(intent);
+  };
+
+  this.startEdit = function() {
+    var file = fileList.file;
+    var intent = new Intent("http://webintents.org/edit", file.type, file);
+    window.navigator.startActivity(intent);
+  };
+})();
+
+var FileListModel = function(files) {
+  var self = this;
+  this.file = undefined;
+  this.selectFile = function(entry) {
+    var url = entry.toURL();
+    entry.file(function(f) {
+      self.fileEntry = entry;
+      self.file = f;
+      if(f.type.substring(0,5) == "image") {
+        $("#viewer")[0].src = url; 
+      }
+    });
+  };
+  this.files = ko.observableArray(files);
+  this.addFile = function(file) {
+    this.files.push(file);
+  }
+};
+
+var fileList;
+var filer = new Filer();
+var size = 1024 * 1024 * 1204;
+filer.init({persistent: true, size: size}, function(fs) {
+  filer.ls('/', function(entries) {
+    fileList = new FileListModel(entries);
+    ko.applyBindings(fileList);
+  });
+});
+
+
