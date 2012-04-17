3133e00e3746d6b1a1aed5609a35fcf294e68b12
Paul Kinlan <paulkinlan@google.com>
2012-04-17 18:56:13 +0100

    Updating to include some prefix information

---
+++ src/webintents-prefix.js
@@ -0,0 +1,3 @@
+window.Intent = window.Intent || window.WebKitIntent;
+window.navigator.startActivity = window.navigator.startActivity || window.navigator.webkitStartActivity;
+window.intent = window.intent || window.webkitIntent;
