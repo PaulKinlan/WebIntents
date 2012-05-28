09401d0d4e05c639ada2a2a0a89aa2231e26274c
Paul Kinlan <paulkinlan@google.com>
2012-05-28 18:37:15 +0100

    Making look a little bit prettier

--- server/demos/mememator/static/scripts/view.js
+++ server/demos/mememator/static/scripts/view.js
@@ -6,24 +6,3 @@ var context;
 var canvas;
 var imageID;
 var permissionKey;
-
-$(function() {
-  
-  $('#save').click(function() {
-    var url = "http://www.mememator.com/image/" + imageID; 
-    var i = new Intent("http://webintents.org/save", "image/*", url);
-    startActivity.call(window.navigator, i);
-  });
-
-  $('#share').click(function() {
-    var url = "http://www.mememator.com/image/" + imageID; 
-    var i = new Intent("http://webintents.org/share", "image/*", url);
-    startActivity.call(window.navigator, i);
-  });
-
-  $('#sharelink').click(function() {
-    var url = "http://www.mememator.com/?id=" + imageID; 
-    var i = new Intent("http://webintents.org/share", "text/uri-list", url);
-    startActivity.call(window.navigator, i);
-  });
-});
