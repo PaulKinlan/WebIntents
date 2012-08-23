46ae3e7b6092b90e8241bc1e2e9ce24ea8fefbd9
Paul Kinlan <paulkinlan@google.com>
2012-08-23 08:53:56 -0700

    Adding previews to intents debugger

--- tools/chrome/extensions/debugger/intent.js
+++ tools/chrome/extensions/debugger/intent.js
@@ -14,9 +14,21 @@ window.addEventListener("load", function() {
          var action = document.getElementById("action");
          var type = document.getElementById("type");
          var data = document.getElementById("data");
+         var majorType = data.className = result.type.substr(0, result.type.indexOf("/"));
+         var typeEl = document.getElementById("majorType");
          action.textContent = result.action;
          type.textContent = result.type;
          data.textContent = result.data;
+
+         if(majorType == "image") {
+           majorType.src = data;
+         }
+         else if (majorType == "video") {
+           majorType.src = data;
+         }
+         else if(majorType == "audio") {
+           majorType.src = data;
+         }
        }
      }
     );    
